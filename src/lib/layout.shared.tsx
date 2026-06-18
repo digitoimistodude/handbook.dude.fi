c965b0dca038c05ee0f1d810c91b8b70bb69f989
Rolle Laukkarinen <roni@dude.fi>
2026-06-18 22:59:04 +0300

    Inline logo SVG with currentColor for theme support, drop wordmark text, Ref: DEV-1094

--- src/lib/layout.shared.tsx
+++ src/lib/layout.shared.tsx
@@ -1,16 +1,11 @@
 import type { BaseLayoutProps } from 'fumadocs-ui/layouts/shared';
 import { gitConfig } from './shared';
+import { DudeLogo } from '@/components/logo';
 
 export function baseOptions(): BaseLayoutProps {
   return {
     nav: {
-      title: (
-        <>
-          {/* eslint-disable-next-line @next/next/no-img-element */}
-          <img src="/dude-logo.svg" alt="Dude" style={{ height: 16, width: 'auto' }} />
-          <span style={{ fontWeight: 600 }}>Handbook</span>
-        </>
-      ),
+      title: <DudeLogo />,
     },
     githubUrl: `https://github.com/${gitConfig.user}/${gitConfig.repo}`,
   };
